fc13d1befbbc679bb027147b30071f5e34402a29
Student Free <l3ra131007@gmail.com>
2024-11-02 15:03:02 +0300

    final commit

--- src/main/java/com/homework/homework25/Application.java
+++ src/main/java/com/homework/homework25/Application.java
@@ -1,4 +1,4 @@
-package com.homework.homework25.model;
+package com.homework.homework25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
